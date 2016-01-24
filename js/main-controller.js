67de3314ef47b95fc419f6d2e268b37c0843541f
Jack Smith <devjacksmith@gmail.com>
2016-01-24 10:00:08 -0700

    Fixed bug #19 of dissapearing mice

--- js/main-controller.js
+++ js/main-controller.js
@@ -18,27 +18,29 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
                 mice: JSON.stringify($scope.micelist),
             }
         }).success(function(response) {
-            // populate $scope.search_results grouped by location
+            var structured_locations = {};
+            // build location objects from mice objects in response
             angular.forEach(response, function(mouse) {
                 if (mouse.is_valid) {
                     $scope.mice_found++;
                     angular.forEach(mouse.locations, function(location, location_id) {
-                        if (!$scope.search_results.hasOwnProperty(location_id)) {
-                            $scope.search_results[location_id] = {};
-                            $scope.search_results[location_id].mice = [];
+                        if (!structured_locations.hasOwnProperty(location_id)) {
+                            structured_locations[location_id] = {};
+                            structured_locations[location_id].mice = [];
                         }
-                        $scope.search_results[location_id].name = location.name;
-                        $scope.search_results[location_id].stage = location.stage;
+                        structured_locations[location_id].name = location.name;
+                        structured_locations[location_id].stage = location.stage;
+                        structured_locations[location_id].id = location_id;
                         var cheeses = [];
                         angular.forEach(mouse.cheeses, function(cheese) {
                             cheeses.push(cheese);
                         });
-                        $scope.search_results[location_id].mice.push({
+                        structured_locations[location_id].mice.push({
                             name: mouse.name,
                             mouse_wiki_url: mouse.mouse_wiki_url,
                             cheeses: cheeses
                         });
-                        $scope.search_results[location_id].size = $scope.search_results[location_id].mice.length;
+                        structured_locations[location_id].size = structured_locations[location_id].mice.length;
                     });
                 } else if (mouse.name != '') {
                     $scope.mice_not_found.push({
@@ -46,6 +48,12 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
                     });
                 }
             });
+
+            // populate $scope.search_results grouped by location
+            angular.forEach(structured_locations, function(location) {
+                $scope.search_results.push(location);
+            });
+
             $('#custom_loader').fadeOut('slow');
             $scope.first_load = false;
         });
@@ -56,6 +64,7 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
         $scope.search_results = [];
         $scope.first_load = true;
         $scope.mice_not_found = [];
+        $scope.mice_found = 0;
     };
     // Remove a mouse from list
     $scope.remove_a_mouse = function(mouse_name) {
