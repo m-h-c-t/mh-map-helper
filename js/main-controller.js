6df758cc503837764de9274f3ea372a64648dc11
Jack Smith <devjacksmith@gmail.com>
2016-01-24 14:03:52 -0700

    Design rework for simpler stages display

--- js/main-controller.js
+++ js/main-controller.js
@@ -24,23 +24,31 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
                 if (mouse.is_valid) {
                     $scope.mice_found++;
                     angular.forEach(mouse.locations, function(location, location_id) {
-                        if (!structured_locations.hasOwnProperty(location_id)) {
-                            structured_locations[location_id] = {};
-                            structured_locations[location_id].mice = [];
+                        if (!structured_locations.hasOwnProperty(location.name)) {
+                            structured_locations[location.name] = {};
+                            structured_locations[location.name].mice = {};
+                            structured_locations[location.name].stages ={};
                         }
-                        structured_locations[location_id].name = location.name;
-                        structured_locations[location_id].stage = location.stage;
-                        structured_locations[location_id].id = location_id;
+                        if (!structured_locations[location.name].stages.hasOwnProperty(location.stage)) {
+                            structured_locations[location.name].stages[location.stage] = {};
+                            structured_locations[location.name].stages[location.stage].mice = [];
+                            structured_locations[location.name].stages[location.stage].size = 0;
+                            structured_locations[location.name].stages[location.stage].id = location_id;
+                        }
+                        structured_locations[location.name].name = location.name;
+
                         var cheeses = [];
                         angular.forEach(mouse.cheeses, function(cheese) {
                             cheeses.push(cheese);
                         });
-                        structured_locations[location_id].mice.push({
+                        structured_locations[location.name].stages[location.stage].mice.push({
                             name: mouse.name,
                             mouse_wiki_url: mouse.mouse_wiki_url,
                             cheeses: cheeses
                         });
-                        structured_locations[location_id].size = structured_locations[location_id].mice.length;
+
+                        structured_locations[location.name].mice[mouse.name] = true;
+                        structured_locations[location.name].size = Object.keys(structured_locations[location.name].mice).length;
                     });
                 } else if (mouse.name != '') {
                     $scope.mice_not_found.push({
@@ -69,13 +77,19 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
     // Remove a mouse from list
     $scope.remove_a_mouse = function(mouse_name) {
         angular.forEach($scope.search_results, function(location) {
-            angular.forEach(location.mice, function(mouse, key) {
-                if (mouse.name === mouse_name) {
-                    location.mice.splice(key, 1);
-                    location.size--;
-                }
+            angular.forEach(location.stages, function(stage, stage_name) {
+                angular.forEach(stage.mice, function(mouse, key) {
+                    if (mouse.name === mouse_name) {
+                        stage.mice.splice(key, 1);
+                    }
+                });
             });
+            location.size--;
         });
+        $scope.mice_found--;
+    };
+    $scope.toggle_stage = function(id) {
+        $('#stage' + id).toggleClass("hide_class");
     };
 }]);
 app.directive('miceListForm', function() {
