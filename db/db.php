3067b5495eb646af15cfcbcdcad410a88e7ccf52
vagrant <devjacksmith@gmail.com>
2016-07-15 17:42:12 +0000

    Cleaned up db files

--- db/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once "__FILE__/../config.php";
-
-$db = new PDO('mysql:host=' . $config['dbhost'] . ';dbname=' . $config['dbname'] . ';charset=utf8', $config['dbuser'], $config['dbpassword']);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$GLOBALS['db'] = $db;
--- sql/migration_to_stages.sql
+++
@@ -1,5 +0,0 @@
-# Import stages table first
-
-UPDATE locations l, stages s
-SET l.stage = s.id
-WHERE l.stage = s.name;
