7b31aa83114a6d1c5ea27999a4d79fe1dac12731
vagrant <devjacksmith@gmail.com>
2016-07-18 19:38:00 +0000

    Added config pages for locations, cheeses and mice

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,18 @@
 Route::get('/', 'SearchController@main');
 
 Route::get('search', 'SearchController@search');
+
+// Config
+Route::get('config', array('middleware' => 'auth.basic', 'uses' => 'ConfigController@main'));
+
+// Config - mice
+Route::post('config/mice/add', array('middleware' => 'auth.basic', 'uses' => 'ConfigController@addMouse'));
+Route::get('config/mice/remove/{mouse}', array('middleware' => 'auth.basic', 'uses' => 'ConfigController@removeMouse'));
+
+// Config - locations
+Route::post('config/locations/add', array('middleware' => 'auth.basic', 'uses' => 'ConfigController@addLocation'));
+Route::get('config/locations/remove/{location}', array('middleware' => 'auth.basic', 'uses' => 'ConfigController@removeLocation'));
+
+// Config - cheeses
+Route::post('config/cheeses/add', array('middleware' => 'auth.basic', 'uses' => 'ConfigController@addCheese'));
+Route::get('config/cheeses/remove/{cheese}', array('middleware' => 'auth.basic', 'uses' => 'ConfigController@removeCheese'));
