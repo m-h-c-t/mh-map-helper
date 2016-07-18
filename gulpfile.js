80e6504f55cea41a1605432dfdce54de1c871190
vagrant <devjacksmith@gmail.com>
2016-07-18 15:07:34 +0000

    Added gulp imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,26 @@
+// Includes
 var elixir = require('laravel-elixir');
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+// Config
 elixir.config.js.browserify.watchify.options.poll = true;
 
-elixir(function(mix) {
+// Process
+elixir.extend('images', function () {
+    new elixir.Task('images', function () {
+        return gulp.src('resources/assets/images/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('public/images'));
+    }).watch('resources/assets/images/*');
+});
+
+elixir(function (mix) {
     mix.sass('custom.scss');
 
     mix.scripts('main-controller.js');
 
-    mix.copy('resources/assets/images', 'public/images');
-        // .version('public/images');
-
     mix.version(['public/css/custom.css', 'public/js/main-controller.js'], 'public/build');
+
+    mix.images();
 });
