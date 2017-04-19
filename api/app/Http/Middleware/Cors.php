df76daea2d2b2878cfcdb950c79dfd5f68a9e9ae
Jack <devjacksmith@gmail.com>
2017-04-19 11:51:10 -0400

    Moved to https and added favicon

--- api/app/Http/Middleware/Cors.php
+++ api/app/Http/Middleware/Cors.php
@@ -8,7 +8,7 @@ class Cors {
     public function handle($request, Closure $next)
     {
         $url = explode('.', $_SERVER['HTTP_HOST']);
-        if ($_SERVER['HTTP_ORIGIN'] == 'http://' . implode('.', [$url[1], $url[2], $url[3]])) {
+        if ($_SERVER['HTTP_ORIGIN'] == 'https://' . implode('.', [$url[1], $url[2], $url[3]])) {
             return $next($request)
                 ->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', 'GET');
