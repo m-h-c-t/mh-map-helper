3f53948f4106d38ba9428c9950ec1f995c98293d
Jack <devjacksmith@gmail.com>
2017-04-29 15:32:06 -0400

    Simplified custom sort for attraction rates

--- front/js/source/main-controller.js
+++ front/js/source/main-controller.js
@@ -219,18 +219,4 @@ app.directive('customHeader', function () {
     };
 });
 
-// Used for attraction rates
-app.filter("emptyToEnd", function () {
-    return function (array, key) {
-        if(!angular.isArray(array)) return;
-        var present = array.filter(function (item) {
-            return item[key];
-        });
-        var empty = array.filter(function (item) {
-            return !item[key]
-        });
-        return present.concat(empty);
-    };
-});
-
 })();
