e4882a9ecf78e06e52ed25d440a6c484afef91a4
Jack <devjacksmith@gmail.com>
2017-04-29 15:25:47 -0400

    Added custom sort for attraction rates

--- front/js/source/main-controller.js
+++ front/js/source/main-controller.js
@@ -219,4 +219,18 @@ app.directive('customHeader', function () {
     };
 });
 
+// Used for attraction rates
+app.filter("emptyToEnd", function () {
+    return function (array, key) {
+        if(!angular.isArray(array)) return;
+        var present = array.filter(function (item) {
+            return item[key];
+        });
+        var empty = array.filter(function (item) {
+            return !item[key]
+        });
+        return present.concat(empty);
+    };
+});
+
 })();
