1ae06e028e7e59b5483ae7bbdd8761b5d9cc2bc0
Jack <devjacksmith@gmail.com>
2017-06-11 20:37:43 -0400

    Added google analytics

--- front/index.php
+++ front/index.php
@@ -37,5 +37,6 @@
     <!-- JS Includes -->
     <script defer src="//code.jquery.com/jquery-1.11.3.min.js"></script>
     <script defer src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <?php include_once("ga.php") ?>
 </body>
 </html>
