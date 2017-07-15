7244aa5580d4f2a072a0aa29c7947483addf9c53
Jack <devjacksmith@gmail.com>
2017-07-15 16:42:42 -0400

    Added fb tags

--- front/index.php
+++ front/index.php
@@ -8,6 +8,12 @@
     <base href="/">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="Jack's MouseHunt Tools" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.agiletravels.com" />
+    <meta property="og:description" content="Tools to help with the MouseHunt game." />
+    <meta property="og:image" content="https://www.agiletravels.com/images/fb_image.jpg" />
+    <meta property="fb:app_id" content="314857368939024" />
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <!-- <link rel="stylesheet" href="css/bootstrap-theme.min.css"> -->
     <link rel="stylesheet" href="css/dist/<?php echo asset_path('custom.min.css'); ?>">
