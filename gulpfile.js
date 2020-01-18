27d9db95129f9f25a2ac703132184fda119ef72b
tehhowch <tehhowch@users.noreply.github.com>
2020-01-18 08:47:22 -0600

    chore(dependencies): update gulp et al
     - update gulpfile `task` usage to new API syntax

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 const livereload = require('gulp-livereload');
 const rev = require('gulp-rev');
-const clean = require('gulp-clean');
+const gulpClean = require('gulp-clean');
 // Includes - js
 const minify = require('gulp-minify');
 // Includes - images
@@ -11,30 +11,32 @@ const imagemin = require('gulp-imagemin');
 // Includes - css
 const cleanCSS = require('gulp-clean-css');
 
-// DEFAULT
-gulp.task('default', function () {
-    gulp.start('img', 'css', 'js');
-});
+/**
+ * Delete files matching the fileglob
+ * @param {string} srcGlob path and glob of files to be deleted by the gulp task.
+ */
+const cleanFiles = (srcGlob) => gulp.src(srcGlob, {read: false}).pipe(gulpClean({force: true}));
 
 // WATCH
 gulp.task('watch', function () {
     livereload.listen();
-    gulp.watch('front/js/source/*.js', {usePolling: true}, ['js']);
-    gulp.watch('front/css/source/*.css', {usePolling: true}, ['css']);
-    gulp.watch('front/images/source/*', {usePolling: true}, ['img']);
+    gulp.watch('front/js/source/*.js', {usePolling: true}, gulp.task('js'));
+    gulp.watch('front/css/source/*.css', {usePolling: true}, gulp.task('css'));
+    gulp.watch('front/images/source/*', {usePolling: true}, gulp.task('img'));
 });
 
 // JAVASCRIPTS
 gulp.task('js-clean', function () {
-    gulp.src('front/js/dist/main-controller*.js', {read: false})
-        .pipe(clean({force: true}));
+    return cleanFiles('front/js/dist/main-controller*.js')
 });
 
-gulp.task('js', ['css', 'js-clean'], function () {
-    gulp.src('node_modules/js-cookie/src/js.cookie.js')
+gulp.task('copy-cookie', function () {
+    return gulp.src('node_modules/js-cookie/src/js.cookie.js')
         .pipe(gulp.dest('front/js/dist'));
+});
 
-    gulp.src('front/js/source/*.js')
+gulp.task('minify-js', function () {
+    return gulp.src('front/js/source/*.js')
         .pipe(minify({
             ext: {
                 src: '.js',
@@ -50,10 +52,11 @@ gulp.task('js', ['css', 'js-clean'], function () {
         .pipe(gulp.dest('api/public'))
         .pipe(livereload());
 });
+gulp.task('js', gulp.series('js-clean', gulp.parallel('copy-cookie', 'minify-js')));
 
 // IMAGES
 gulp.task('img', function () {
-    gulp.src('front/images/source/*')
+    return gulp.src('front/images/source/*')
         .pipe(imagemin())
         .pipe(gulp.dest('front/images/dist'))
         .pipe(gulp.dest('api/public/images/dist'))
@@ -61,7 +64,15 @@ gulp.task('img', function () {
 });
 
 // CSS
-gulp.task('css', ['css-clean'], function () {
+const clean_frontend_css = () => cleanFiles('front/css/dist/custom*.css');
+const clean_api_css = () => cleanFiles('api/public/css/dist/custom*.css');
+
+gulp.task('css-clean', gulp.parallel(
+    clean_frontend_css,
+    clean_api_css
+));
+
+gulp.task('css', gulp.series('css-clean', function css_update() {
     return gulp.src('front/css/source/*.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(rename({
@@ -76,11 +87,7 @@ gulp.task('css', ['css-clean'], function () {
         .pipe(gulp.dest('front'))
         .pipe(gulp.dest('api/public'))
         .pipe(livereload());
-});
+}));
 
-gulp.task('css-clean', function () {
-    gulp.src('front/css/dist/custom*.css', {read: false})
-        .pipe(clean({force: true}));
-    gulp.src('api/public/css/dist/custom*.css', {read: false})
-        .pipe(clean({force: true}));
-});
+// DEFAULT
+gulp.task('default', gulp.parallel('img', 'css', 'js'));
